2552735cebfcc4da38e3cee2fba63c4fcf74c182
Kinas Maria <kinasmaria01@gmail.com>
2022-07-06 13:12:26 +0300

    dropbox help

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import DropBoxHelp from './components/DropBoxHelp'
 import MainScreen from './components/MainScreen'
 import MoreInfo from './components/MoreInfo'
 import MoreOpportunity from './components/MoreOpportunity'
@@ -9,6 +10,7 @@ function MainPage() {
       <MainScreen />
       <MoreOpportunity />
       <MoreInfo />
+      <DropBoxHelp />
     </div>
   )
 }
