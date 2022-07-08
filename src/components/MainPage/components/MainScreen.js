dfe971677e0c0d47280d88542036ae7aeb121969
Kinas Maria <kinasmaria01@gmail.com>
2022-07-08 14:41:12 +0300

    use DropBox for...

--- src/components/MainPage/components/MainScreen.js
+++ src/components/MainPage/components/MainScreen.js
@@ -13,7 +13,7 @@ function MainScreen() {
             </div>
             <p className='main-title'>Тримайте. Синхронізуйте. Діліться. Підписуйте. Все просто.</p>
             <p className='main-text'>Новий пакет Dropbox Professional + eSign: місце зберігання, електронні підписи та інші корисні функції.</p>
-            <button className='header-button'>Пізнайте більше<i class="fas fa-angle-right"></i></button>
+            <button className='button'>Пізнайте більше<i class="fas fa-angle-right"></i></button>
         </div>
             <img src="http://fotorelax.ru/wp-content/uploads/2016/02/Beautiful-photos-and-pictures-on-different-topics-01.jpg" alt="" />
     </div>
